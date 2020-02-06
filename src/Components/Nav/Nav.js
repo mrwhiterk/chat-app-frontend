566b12b48d07c3ed3e5abfc821d68f52eab021f9
Emma Vanninen <50673037+emmavanninen@users.noreply.github.com>
2020-02-06 15:18:32 -0500

    Update Nav.js

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -26,14 +26,14 @@ export default class Nav extends Component {
 
         {/* Register / Login tabs */}
         <Tabs>
-          <Tab className="login-tab" lable="Login">
+          <Tab className="login-tab" label="Login">
             <div>
               <div className="tabContent">
                 {!this.context.isAuth && <LoginForm />}
               </div>
             </div>
           </Tab>
-          <Tab className="register-log" lable="Register">
+          <Tab className="register-log" label="Register">
             <div>
               <div className="tabContent">
                 {!this.context.isAuth && <RegisterForm />}
