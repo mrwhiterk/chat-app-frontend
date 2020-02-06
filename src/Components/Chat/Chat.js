7ba3fdba5fa6954b51104df0d12c421a7b0393d0
Ryan White <mrwhiterk@gmail.com>
2020-02-06 16:55:03 -0500

    started working on sockets integration

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,15 +1,31 @@
-import React, { Component } from "react";
-import ChatBoard from "./ChatBoard/ChatBoard";
-import ChatSidebar from "./ChatSidebar/ChatSidebar";
-import "./Chat.css";
+import React, { Component } from 'react'
+import ChatBoard from './ChatBoard/ChatBoard'
+import ChatSidebar from './ChatSidebar/ChatSidebar'
+import socketIOClient from 'socket.io-client'
+import './Chat.css'
 
 export default class Chat extends Component {
+  state = {
+    responses: [],
+    message: 'hey',
+    endpoint: 'http://127.0.0.1:3001'
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state
+    console.log(endpoint)
+
+    const socket = socketIOClient(endpoint)
+
+    socket.emit('message', this.state.message)
+  }
+
   render() {
     return (
       <div className="chatMain">
         <ChatBoard />
         <ChatSidebar />
       </div>
-    );
+    )
   }
 }
