2a649e4af0eeac537c99cb6b4dd46590760f0e5d
Ryan White <mrwhiterk@gmail.com>
2020-02-11 15:52:27 -0500

    added Tab Content to Tabs

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,37 +1,37 @@
-import React, { Component } from "react";
-import RegisterForm from "./TabContent/RegisterForm/RegisterForm";
-import LoginForm from "./TabContent/LoginForm/LoginForm";
-import Context from "../Context/Context";
-import Tabs from "./Tabs/Tabs";
-import Tab from "./Tabs/Tab";
-import "./Nav.css";
+import React, { Component } from 'react'
+import RegisterForm from './Tabs/TabContent/RegisterForm/RegisterForm'
+import LoginForm from './Tabs/TabContent/LoginForm/LoginForm'
+import Context from '../Context/Context'
+import Tabs from './Tabs/Tabs'
+import Tab from './Tabs/Tab'
+import './Nav.css'
 
 const errorToastColor = {
-  background: "#f23535",
-  text: "#fff"
-};
+  background: '#f23535',
+  text: '#fff'
+}
 const toastColor = {
-  background: "#3f51b5",
-  text: "#fff"
-};
+  background: '#3f51b5',
+  text: '#fff'
+}
 
 export default class Nav extends Component {
-  static contextType = Context;
+  static contextType = Context
 
   state = {
     notification: null
-  };
+  }
 
   componentDidMount() {
     if (this.context.toastMsg.success) {
       this.setState({
         notification: this.context.toastMsg.success
-      });
+      })
     }
     if (this.context.toastMsg.error) {
       this.setState({
         notification: this.context.toastMsg.error
-      });
+      })
     }
   }
 
@@ -70,6 +70,6 @@ export default class Nav extends Component {
           </Tab>
         </Tabs>
       </div>
-    );
+    )
   }
 }
--- src/Components/Nav/Tabs/TabContent/RegisterForm/RegisterForm.js
+++ src/Components/Nav/Tabs/TabContent/RegisterForm/RegisterForm.js
@@ -1,56 +1,56 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import {
   signup,
   setAuthHeader,
   checkTokenAndReturn
-} from "../../../../api/axios-helpers";
-import Context from "../../../Context/Context";
+} from '../../../../../api/axios-helpers'
+import Context from '../../../../Context/Context'
 
 class RegisterForm extends Component {
-  static contextType = Context;
+  static contextType = Context
 
   state = {
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: ""
-  };
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  }
 
   handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+    this.setState({ [e.target.name]: e.target.value })
+  }
 
   handleSubmit = async e => {
-    e.preventDefault();
+    e.preventDefault()
 
     if (this.state.password !== this.state.confirmPassword) {
-      console.log("password and confirm password don't match");
-      return;
+      console.log("password and confirm password don't match")
+      return
     }
     try {
-      let res = await signup(this.state);
+      let res = await signup(this.state)
 
       if (res.status === 200) {
-        setAuthHeader(res.data.token);
-        localStorage.setItem("token", res.data.token);
-        this.context.setAuth(checkTokenAndReturn());
+        setAuthHeader(res.data.token)
+        localStorage.setItem('token', res.data.token)
+        this.context.setAuth(checkTokenAndReturn())
       }
 
       if (res.status === 400) {
-        let { errors } = res.data;
+        let { errors } = res.data
         if (errors) {
-          if (errors.username && errors.username.kind === "unique") {
-            console.log("username taken");
+          if (errors.username && errors.username.kind === 'unique') {
+            console.log('username taken')
           }
-          if (errors.email && errors.email.kind === "unique") {
-            console.log("email taken");
+          if (errors.email && errors.email.kind === 'unique') {
+            console.log('email taken')
           }
         }
       }
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
-  };
+  }
 
   render() {
     let form = (
@@ -103,9 +103,9 @@ class RegisterForm extends Component {
           <input className="navButton" type="submit" value="Register" />
         </form>
       </div>
-    );
-    return form;
+    )
+    return form
   }
 }
 
-export default RegisterForm;
+export default RegisterForm
