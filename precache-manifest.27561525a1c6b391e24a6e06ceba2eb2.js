d83b2185b387068b6bf2f1c33f5c249739f5dceb
Ryan White <mrwhiterk@gmail.com>
2020-02-14 16:38:03 -0500

    Updates

---
+++ precache-manifest.27561525a1c6b391e24a6e06ceba2eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2204daebe01ec2db4cfa5550842f23d",
+    "url": "/chat-app-frontend/index.html"
+  },
+  {
+    "revision": "131de3e07d93706f0949",
+    "url": "/chat-app-frontend/static/css/main.689218d7.chunk.css"
+  },
+  {
+    "revision": "10b1369df4ff3002f66e",
+    "url": "/chat-app-frontend/static/js/2.4f0163ac.chunk.js"
+  },
+  {
+    "revision": "e2a595cebc19315c6e6690cf04f42e5b",
+    "url": "/chat-app-frontend/static/js/2.4f0163ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "131de3e07d93706f0949",
+    "url": "/chat-app-frontend/static/js/main.005a1ed0.chunk.js"
+  },
+  {
+    "revision": "cf3195dc7909fdaf9354",
+    "url": "/chat-app-frontend/static/js/runtime-main.ad0ca5b2.js"
+  }
+]);
