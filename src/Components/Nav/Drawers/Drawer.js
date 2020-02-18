18c0817ecca08bd971435c07a1cdd8589a7cae77
emmavanninen <emmavanninen@gmail.com>
2020-02-18 10:57:16 -0500

    save update

--- src/Components/Nav/Drawers/Drawer.js
+++ src/Components/Nav/Drawers/Drawer.js
@@ -8,34 +8,28 @@ export default class Drawer extends Component {
     return (
       <Trigger>
         <Box>
-                <FadeInAndOut fadeout={false}>
-          <div className="drawer">
-            <div
-              className={`drawerLink ${this.props.className}
+          <FadeInAndOut fadeout={false}>
+            <div className="drawer">
+              <div
+                className={`drawerLink ${this.props.className}
         ${this.props.isActive}
         `}
-              onClick={event => {
-                event.preventDefault()
-                // this.props.onClick(this.props.drawer)
-              }}
-              div
-            >
-              {`${this.props.label}`}
+                onClick={event => {
+                  event.preventDefault()
+                  // this.props.onClick(this.props.drawer)
+                }}
+                div
+              >
+                {`${this.props.label}`}
+              </div>
+              {/* {this.props.isActive */}
+              {/* ? */}
+              {React.Children.map(this.props.children, (child, i) => {
+                return <div className="activeDrawerContent">{child}</div>
+              })}
+              {/* : ""} */}
             </div>
-            {/* {this.props.isActive */}
-            {/* ? */}
-            {React.Children.map(this.props.children, (child, i) => {
-              return (
-                <div className="activeDrawerContent">
-                  
-                  {child}
-                  
-                </div>
-              )
-            })}
-            {/* : ""} */}
-          </div>
-                </FadeInAndOut>
+          </FadeInAndOut>
         </Box>
       </Trigger>
     )
