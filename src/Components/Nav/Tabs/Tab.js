7f25b22f8e2a3ecf97d4dd478b848afca3e1b919
emmavanninen <emmavanninen@gmail.com>
2020-02-19 14:43:51 -0500

    profile info + username

--- src/Components/Nav/Tabs/Tab.js
+++ src/Components/Nav/Tabs/Tab.js
@@ -1,14 +1,26 @@
-import React, { Component } from "react"
-import "./Tabs.css"
+import React, { Component } from 'react'
+import './Tabs.css'
 
 export default class Tab extends Component {
   render() {
     return (
       <>
         <div
-          className="tab"
+          className='tab'
           style={{
-            backgroundColor: this.props.isActive ? "#262626" : "#161515"
+            backgroundColor: this.props.isActive ? '#141314' : '#0c0b0c',
+            borderBottom: this.props.isActive
+              ? 'none'
+              : '1px solid rgb(63, 60, 61)',
+            borderTop: this.props.isActive
+              ? '1px solid rgb(63, 60, 61)'
+              : 'none',
+            borderLeft: this.props.isActive
+              ? '1px solid rgb(63, 60, 61)'
+              : 'none',
+            borderRight: this.props.isActive
+              ? '1px solid rgb(63, 60, 61)'
+              : 'none'
           }}
         >
           <p
